452aa6f60b1a3127d0d7f3abdd19727fdd245387
maurobortolani <60657059+maurobortolani@users.noreply.github.com>
2023-11-06 22:21:24 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace PONG3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBoxCampo = new PictureBox();
+            textBoxX = new TextBox();
+            labelX = new Label();
+            labelY = new Label();
+            textBoxY = new TextBox();
+            labelYDir = new Label();
+            textBoxYDir = new TextBox();
+            labelXdir = new Label();
+            textBoxXDir = new TextBox();
+            labelColor = new Label();
+            textBoxColore = new TextBox();
+            labelDim = new Label();
+            textBoxDim = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            textBoxGiocatori = new TextBox();
+            textBoxPalline = new TextBox();
+            labelGiocatori = new Label();
+            labelPalline = new Label();
+            buttonGenerate = new Button();
+            buttonRun = new Button();
+            labelPunteggioA = new Label();
+            labelPunteggioB = new Label();
+            textBoxKey = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCampo).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxCampo
+            // 
+            pictureBoxCampo.BackColor = Color.White;
+            pictureBoxCampo.Location = new Point(12, 12);
+            pictureBoxCampo.Name = "pictureBoxCampo";
+            pictureBoxCampo.Size = new Size(803, 374);
+            pictureBoxCampo.TabIndex = 0;
+            pictureBoxCampo.TabStop = false;
+            // 
+            // textBoxX
+            // 
+            textBoxX.Location = new Point(915, 12);
+            textBoxX.Name = "textBoxX";
+            textBoxX.Size = new Size(100, 23);
+            textBoxX.TabIndex = 1;
+            // 
+            // labelX
+            // 
+            labelX.AutoSize = true;
+            labelX.Location = new Point(871, 15);
+            labelX.Name = "labelX";
+            labelX.Size = new Size(13, 15);
+            labelX.TabIndex = 2;
+            labelX.Text = "x";
+            // 
+            // labelY
+            // 
+            labelY.AutoSize = true;
+            labelY.Location = new Point(871, 44);
+            labelY.Name = "labelY";
+            labelY.Size = new Size(13, 15);
+            labelY.TabIndex = 4;
+            labelY.Text = "y";
+            // 
+            // textBoxY
+            // 
+            textBoxY.Location = new Point(915, 41);
+            textBoxY.Name = "textBoxY";
+            textBoxY.Size = new Size(100, 23);
+            textBoxY.TabIndex = 3;
+            // 
+            // labelYDir
+            // 
+            labelYDir.AutoSize = true;
+            labelYDir.Location = new Point(871, 102);
+            labelYDir.Name = "labelYDir";
+            labelYDir.Size = new Size(28, 15);
+            labelYDir.TabIndex = 8;
+            labelYDir.Text = "yDir";
+            // 
+            // textBoxYDir
+            // 
+            textBoxYDir.Location = new Point(915, 99);
+            textBoxYDir.Name = "textBoxYDir";
+            textBoxYDir.Size = new Size(100, 23);
+            textBoxYDir.TabIndex = 7;
+            // 
+            // labelXdir
+            // 
+            labelXdir.AutoSize = true;
+            labelXdir.Location = new Point(871, 73);
+            labelXdir.Name = "labelXdir";
+            labelXdir.Size = new Size(28, 15);
+            labelXdir.TabIndex = 6;
+            labelXdir.Text = "xDir";
+            // 
+            // textBoxXDir
+            // 
+            textBoxXDir.Location = new Point(915, 70);
+            textBoxXDir.Name = "textBoxXDir";
+            textBoxXDir.Size = new Size(100, 23);
+            textBoxXDir.TabIndex = 5;
+            // 
+            // labelColor
+            // 
+            labelColor.AutoSize = true;
+            labelColor.Location = new Point(871, 160);
+            labelColor.Name = "labelColor";
+            labelColor.Size = new Size(42, 15);
+            labelColor.TabIndex = 12;
+            labelColor.Text = "Colore";
+            // 
+            // textBoxColore
+            // 
+            textBoxColore.Location = new Point(915, 157);
+            textBoxColore.Name = "textBoxColore";
+            textBoxColore.Size = new Size(100, 23);
+            textBoxColore.TabIndex = 11;
+            // 
+            // labelDim
+            // 
+            labelDim.AutoSize = true;
+            labelDim.Location = new Point(871, 131);
+            labelDim.Name = "labelDim";
+            labelDim.Size = new Size(28, 15);
+            labelDim.TabIndex = 10;
+            labelDim.Text = "dim";
+            // 
+            // textBoxDim
+            // 
+            textBoxDim.Location = new Point(915, 128);
+            textBoxDim.Name = "textBoxDim";
+            textBoxDim.Size = new Size(100, 23);
+            textBoxDim.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // textBoxGiocatori
+            // 
+            textBoxGiocatori.Location = new Point(915, 259);
+            textBoxGiocatori.Name = "textBoxGiocatori";
+            textBoxGiocatori.Size = new Size(100, 23);
+            textBoxGiocatori.TabIndex = 13;
+            // 
+            // textBoxPalline
+            // 
+            textBoxPalline.Location = new Point(915, 288);
+            textBoxPalline.Name = "textBoxPalline";
+            textBoxPalline.Size = new Size(100, 23);
+            textBoxPalline.TabIndex = 14;
+            // 
+            // labelGiocatori
+            // 
+            labelGiocatori.AutoSize = true;
+            labelGiocatori.Location = new Point(854, 262);
+            labelGiocatori.Name = "labelGiocatori";
+            labelGiocatori.Size = new Size(55, 15);
+            labelGiocatori.TabIndex = 15;
+            labelGiocatori.Text = "Giocatori";
+            // 
+            // labelPalline
+            // 
+            labelPalline.AutoSize = true;
+            labelPalline.Location = new Point(854, 291);
+            labelPalline.Name = "labelPalline";
+            labelPalline.Size = new Size(57, 15);
+            labelPalline.TabIndex = 16;
+            labelPalline.Text = "n° Palline";
+            // 
+            // buttonGenerate
+            // 
+            buttonGenerate.Location = new Point(940, 317);
+            buttonGenerate.Name = "buttonGenerate";
+            buttonGenerate.Size = new Size(75, 23);
+            buttonGenerate.TabIndex = 17;
+            buttonGenerate.Text = "Genera";
+            buttonGenerate.UseVisualStyleBackColor = true;
+            buttonGenerate.Click += buttonGenerate_Click;
+            // 
+            // buttonRun
+            // 
+            buttonRun.Location = new Point(940, 466);
+            buttonRun.Name = "buttonRun";
+            buttonRun.Size = new Size(75, 23);
+            buttonRun.TabIndex = 18;
+            buttonRun.Text = "Run";
+            buttonRun.UseVisualStyleBackColor = true;
+            buttonRun.Click += buttonRun_Click;
+            // 
+            // labelPunteggioA
+            // 
+            labelPunteggioA.AutoSize = true;
+            labelPunteggioA.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            labelPunteggioA.Location = new Point(48, 389);
+            labelPunteggioA.Name = "labelPunteggioA";
+            labelPunteggioA.Size = new Size(28, 32);
+            labelPunteggioA.TabIndex = 19;
+            labelPunteggioA.Text = "0";
+            // 
+            // labelPunteggioB
+            // 
+            labelPunteggioB.AutoSize = true;
+            labelPunteggioB.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            labelPunteggioB.Location = new Point(787, 389);
+            labelPunteggioB.Name = "labelPunteggioB";
+            labelPunteggioB.Size = new Size(28, 32);
+            labelPunteggioB.TabIndex = 20;
+            labelPunteggioB.Text = "0";
+            // 
+            // textBoxKey
+            // 
+            textBoxKey.Location = new Point(915, 186);
+            textBoxKey.Name = "textBoxKey";
+            textBoxKey.Size = new Size(100, 23);
+            textBoxKey.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1027, 501);
+            Controls.Add(textBoxKey);
+            Controls.Add(labelPunteggioB);
+            Controls.Add(labelPunteggioA);
+            Controls.Add(buttonRun);
+            Controls.Add(buttonGenerate);
+            Controls.Add(labelPalline);
+            Controls.Add(labelGiocatori);
+            Controls.Add(textBoxPalline);
+            Controls.Add(textBoxGiocatori);
+            Controls.Add(labelColor);
+            Controls.Add(textBoxColore);
+            Controls.Add(labelDim);
+            Controls.Add(textBoxDim);
+            Controls.Add(labelYDir);
+            Controls.Add(textBoxYDir);
+            Controls.Add(labelXdir);
+            Controls.Add(textBoxXDir);
+            Controls.Add(labelY);
+            Controls.Add(textBoxY);
+            Controls.Add(labelX);
+            Controls.Add(textBoxX);
+            Controls.Add(pictureBoxCampo);
+            Name = "Form1";
+            Text = "Pong - 2IoT";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCampo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxCampo;
+        private TextBox textBoxX;
+        private Label labelX;
+        private Label labelY;
+        private TextBox textBoxY;
+        private Label labelYDir;
+        private TextBox textBoxYDir;
+        private Label labelXdir;
+        private TextBox textBoxXDir;
+        private Label labelColor;
+        private TextBox textBoxColore;
+        private Label labelDim;
+        private TextBox textBoxDim;
+        private System.Windows.Forms.Timer timer1;
+        private TextBox textBoxGiocatori;
+        private TextBox textBoxPalline;
+        private Label labelGiocatori;
+        private Label labelPalline;
+        private Button buttonGenerate;
+        private Button buttonRun;
+        private Label labelPunteggioA;
+        private Label labelPunteggioB;
+        private TextBox textBoxKey;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,380 @@
+using System.Drawing;
+using System.Drawing.Design;
+using System.Windows.Forms;
+
+namespace PONG3
+{
+    public partial class Form1 : Form
+    {
+        List<pallina> listBall;
+        List<padella> listPadelle;
+        List<tasti> coppieTasti;
+        Bitmap immagine;
+        int punteggioA = 0, punteggioB = 0;
+        int nA;
+        int nB;
+
+        class tasti
+        {
+            public Keys Up;
+            public Keys Down;
+        }
+        class pallina
+        {
+            public int x, y;
+            public int xDir, yDir;
+            public int dim;
+            public Color colore;
+            public int v;  // velocità
+        }
+        class padella
+        {
+            public int x, y;
+            public int lunghezza, larghezza;
+            public int v;
+            public tasti t;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            immagine = new Bitmap(this.Width, this.Height);
+            listaTasti();
+            textBoxGiocatori.Text = "2";
+            textBoxPalline.Text = "1";
+            disegnaMetaCampo();
+            pictureBoxCampo.Image = immagine;
+        }
+
+        private void listaTasti()
+        {
+            coppieTasti = new List<tasti>();
+            coppieTasti.Add(new tasti { Up = Keys.Q, Down = Keys.A });
+            coppieTasti.Add(new tasti { Up = Keys.W, Down = Keys.S });
+            coppieTasti.Add(new tasti { Up = Keys.O, Down = Keys.L });
+            coppieTasti.Add(new tasti { Up = Keys.I, Down = Keys.K });
+            coppieTasti.Add(new tasti { Up = Keys.D1, Down = Keys.D2 });
+        }
+        private void drawPadella(padella p, Color c)
+        {
+
+            for (int h = 0; h < p.lunghezza; h++)
+            {
+                for (int w = -p.larghezza / 2; w < p.larghezza / 2; w++)
+                {
+                    padella z = tosaCoordinatePadella(p);
+                    immagine.SetPixel(z.x + w, z.y + h, c);
+                }
+            }
+
+            pictureBoxCampo.Image = immagine;
+        }
+
+        private pallina tosaCoordinatePallina(pallina p)
+        {
+            if (p.x <= (p.dim / 2)) p.x = (p.dim / 2);
+            if (p.y <= (p.dim / 2)) p.y = (p.dim / 2);
+            if (p.x > pictureBoxCampo.Width - 1 - (p.dim / 2)) p.x = pictureBoxCampo.Width - 1 - (p.dim / 2);
+            if (p.y > pictureBoxCampo.Height - 1 - (p.dim / 2)) p.y = pictureBoxCampo.Height - 1 - (p.dim / 2);
+            return p;
+        }
+
+        private padella tosaCoordinatePadella(padella p)
+        {
+            if (p.x <= 0) p.x = 0;
+            if (p.y <= 0) p.y = 0;
+            if (p.x > pictureBoxCampo.Width - 1) p.x = pictureBoxCampo.Width - 1;
+            if (p.y > pictureBoxCampo.Height - 1 - p.lunghezza) p.y = pictureBoxCampo.Height - 1 - p.lunghezza;
+            return p;
+        }
+
+        private void instanziaPadelle(int n)
+        {
+            Random rnd = new Random();
+            listPadelle = new List<padella>();
+
+            nA = n / 2;
+            nB = n - nA;
+
+            int sicurezza = 40;
+
+            int metaCampo = pictureBoxCampo.Width / 2;
+
+            int i = 0;
+            if (nA > 0)
+            {
+                int distanzaA = (metaCampo - sicurezza) / nA;
+                for (i = 0; i < nA; i++)
+                {
+                    int l = 100;
+                    listPadelle.Add(new padella
+                    {
+                        lunghezza = l,
+                        y = rnd.Next(0, pictureBoxCampo.Height - l),
+                        larghezza = 3, //rnd.Next(3, 5),
+                        x = sicurezza + i * distanzaA,
+                        t = coppieTasti[i]
+                    });
+                }
+            }
+            if (nB > 0)
+            {
+                int distanzaB = (metaCampo - sicurezza) / nB;
+                for (int j = 0; j < nB; j++)
+                {
+                    int l = 100;
+                    listPadelle.Add(new padella
+                    {
+                        lunghezza = l,
+                        y = rnd.Next(0, pictureBoxCampo.Height - l),
+                        larghezza = rnd.Next(3, 5),
+                        x = pictureBoxCampo.Width - sicurezza - j * distanzaB,
+                        t = coppieTasti[j + i]
+                    });
+                }
+            }
+        }
+
+        private void debugPallina(pallina p)
+        {
+            textBoxX.Text = p.x.ToString();
+            textBoxY.Text = p.y.ToString();
+            textBoxXDir.Text = p.xDir.ToString();
+            textBoxYDir.Text = p.yDir.ToString();
+            textBoxDim.Text = p.dim.ToString();
+            textBoxColore.Text = p.colore.ToString();
+        }
+
+        private void instanziaPalline(int n)
+        {
+            Random rnd = new Random();
+            listBall = new List<pallina>();
+
+            if (n > 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    int yDirTemp;
+                    do
+                        yDirTemp = rnd.Next(-5, 5);
+                    while (yDirTemp == 0);
+
+                    listBall.Add(new pallina
+                    {
+                        colore = Color.Black,
+                        x = rnd.Next(10, pictureBoxCampo.Width - 1),
+                        y = rnd.Next(10, pictureBoxCampo.Height - 1),
+                        xDir = rnd.Next(-5, 5),
+                        yDir = yDirTemp,
+                        dim = 7,//rnd.Next(1, 10),
+                        v = rnd.Next(1, 2)
+                    });
+                }
+            }
+        }
+
+        private void drawPallina(pallina p, Color c)
+        {
+            debugPallina(p);
+            int x, y;
+            for (int i = -p.dim / 2; i < p.dim / 2; i++)
+            {
+                for (int j = -p.dim / 2; j < p.dim / 2; j++)
+                {
+                    x = p.x + i;
+                    if (x < p.dim / 2) x = p.dim / 2;
+                    if (x > pictureBoxCampo.Width - 1 - p.dim / 2)
+                        x = pictureBoxCampo.Width - 1 - p.dim / 2;
+                    y = p.y + j;
+                    if (y < p.dim / 2) y = p.dim / 2;
+                    if (y > pictureBoxCampo.Height - 1 - p.dim / 2)
+                        y = pictureBoxCampo.Height - 1 - p.dim / 2;
+                    immagine.SetPixel(x, y, c);
+                }
+            }
+            pictureBoxCampo.Image = immagine;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (pallina p in listBall)
+            {
+                drawPallina(p, Color.White);
+                p.x += p.xDir / p.v;
+                p.y += p.yDir / p.v;
+                pallina z;
+                z = checkConfineBall(p);
+                z = checkCollision(z);
+                drawPallina(z, z.colore);
+            }
+
+            foreach (padella p in listPadelle)
+            {
+                drawPadella(p, Color.White);
+
+                padella z;
+                z = checkConfinePadella(p);
+                drawPadella(z, Color.Black);
+            }
+
+            disegnaMetaCampo();
+
+            labelPunteggioA.Text = punteggioA.ToString();
+            labelPunteggioB.Text = punteggioB.ToString();
+        }
+
+        private void disegnaMetaCampo()
+        {
+            int x = pictureBoxCampo.Width / 2;
+
+            for (int y = 0; y < pictureBoxCampo.Height; y++)
+            {
+
+                immagine.SetPixel(x, y,
+                    (y / 20) % 2 == 0 ? Color.Black : Color.White);
+            }
+            pictureBoxCampo.Image = immagine;
+        }
+        private pallina checkCollision(pallina p)
+        {
+            foreach (padella padella in listPadelle)
+            {
+                if (
+                        (
+                            p.x >= (padella.x - Math.Abs(p.xDir / 2)) &&
+                            p.x <= (padella.x + Math.Abs(p.xDir / 2) + (padella.larghezza / 2))
+                        ) &&
+                        (
+                            p.y >= padella.y &&
+                            p.y < (padella.y + padella.lunghezza)
+                        )
+                    )
+                {
+                    int d = (padella.x + (padella.lunghezza / 2) - p.x) / 10;
+
+                    p.xDir = p.xDir > 0 ? -d : d;
+
+                    //p.xDir *= (-1);
+                }
+            }
+            return p;
+        }
+
+        private padella checkConfinePadella(padella p)
+        {
+            if (p.y <= 4) p.y = 4;
+            if (p.y > pictureBoxCampo.Height - p.lunghezza) p.y = pictureBoxCampo.Height - p.lunghezza;
+            return p;
+        }
+
+        private pallina checkConfineBall(pallina p)
+        {
+            if (p.x <= p.dim / 2 || p.x >= pictureBoxCampo.Width - 1 - (p.dim / 2))
+            {
+                Random rnd = new Random();
+                p.xDir *= (-1);
+                if (p.x < pictureBoxCampo.Width / 2)
+                {
+                    punteggioB++;
+                    int newPadella = rnd.Next(0, nB - 1);
+                    p.x = listPadelle[nA + newPadella].x - listPadelle[nA + newPadella].larghezza;
+                    p.y = listPadelle[nA + newPadella].y + listPadelle[nA + newPadella].lunghezza / 2;
+                    p.xDir = rnd.Next(-5, 5);
+                    p.yDir = rnd.Next(-5, 5);
+                }
+                else
+                {
+                    punteggioA++;
+                    int newPadella = rnd.Next(0, nA - 1);
+                    p.x = listPadelle[newPadella].x + listPadelle[newPadella].larghezza;
+                    p.y = listPadelle[newPadella].y + listPadelle[newPadella].lunghezza / 2;
+                    p.xDir = rnd.Next(-5, 5);
+                    p.yDir = rnd.Next(-5, 5);
+                }
+                changeRun();
+            }
+
+            if (p.y <= p.dim / 2 || p.y >= pictureBoxCampo.Height - 1 - (p.dim / 2))
+            {
+                p.yDir *= (-1);
+            }
+
+            return p;
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            clearImage(immagine);
+            instanziaPalline(Convert.ToInt32(textBoxPalline.Text));
+            instanziaPadelle(Convert.ToInt32(textBoxGiocatori.Text));
+
+            foreach (pallina p in listBall)
+            {
+                drawPallina(p, p.colore);
+            }
+
+            foreach (padella p in listPadelle)
+            {
+                drawPadella(p, Color.Black);
+            }
+            disegnaMetaCampo();
+        }
+
+        private void clearImage(Bitmap bmp)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    bmp.SetPixel(x, y, Color.White);
+                }
+            }
+            pictureBoxCampo.Image = bmp;
+            immagine = bmp;
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            changeRun();
+        }
+
+        private void changeRun()
+        {
+
+            if (timer1.Enabled == false)
+            {
+                buttonRun.Text = "Stop";
+                timer1.Enabled = true;
+                this.KeyPreview = true;
+            }
+            else
+            {
+                buttonRun.Text = "Run";
+                timer1.Enabled = false;
+                this.KeyPreview = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show(e.KeyCode.ToString());
+
+            foreach (padella p in listPadelle)
+            {
+                drawPadella(p, Color.White);
+                textBoxKey.Text = e.KeyCode.ToString();
+
+                if (e.KeyCode == p.t.Up) p.y -= 10;
+                if (e.KeyCode == p.t.Down) p.y += 10;
+
+                drawPadella(p, Color.Black);
+            }
+
+        }
+
+
+    }
+}
